81218cc878cae495942a45e9c5180c2ad393864b
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-11-14 15:52:08 +0100

    toggling done things

--- app.js
+++ app.js
@@ -18,16 +18,35 @@ const addNote = (e) => {
 };
 
 function printNotes(notes = [], notesHTML) {
-    notesHTML.innerHTML = notes.map((note, i) => {
-      return `
-        <li>
-          <input type="checkbox" data-index=${i} id="item${i}" ${note.done ? 'class="done" checked' : 'class="dx"'} />
-          <label for="item${i}">${note.text}</label>
-        </li>
-        `;
-    }).join('');
+    notesHTML.innerHTML = notes.reduce((html,note, i) => {
+        return html +=`
+          <li ${note.done ? 'class="done"' : ''}>
+            <input type="checkbox" data-index=${i} id="item${i}" ${note.done ? 'class="done" checked' : ''} />
+            <label for="item${i}">${note.text}</label>
+          </li>
+          `;
+      }, ``);
+    // notesHTML.innerHTML = notes.map((note, i) => {
+    //   return `
+    //     <li ${note.done ? 'class="done"' : ''}>
+    //       <input type="checkbox" data-index=${i} id="item${i}" ${note.done ? 'class="done" checked' : ''} />
+    //       <label for="item${i}">${note.text}</label>
+    //     </li>
+    //     `;
+    // }).join('');
   };
 
+  function toggleDone(e) {
+    if (!e.target.matches('input')) return; 
+    const el = e.target;
+    const index = el.dataset.index;
+    notes[index].done = !notes[index].done;
+    localStorage.setItem('notes', JSON.stringify(notes));
+    printNotes(notes, notesHTML)
+    //document.querySelectorAll('li')[index].classList.toggle("done", notes[index].done);
+}
 
 document.querySelector(".add-notes").addEventListener("submit", addNote);
-printNotes(notes, notesHTML)
+document.querySelector(".toDoList").addEventListener("click", toggleDone);
+
+printNotes(notes, notesHTML);
